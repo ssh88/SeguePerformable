f84cd0f034ddde6a7b5beb3bfcdee7f69c1f6d3b
Shabeer Hussain <shabeershussain@gmail.com>
2018-09-14 21:23:38 +0100

    Update SeguePerformable.swift

--- SeguePerformable.swift
+++ SeguePerformable.swift
@@ -11,36 +11,38 @@ import UIKit
 
 protocol SeguePerformable {
     associatedtype SegueIdentifier: RawRepresentable
-    typealias SegueCompletionBlock = (UIStoryboardSegue, UIViewController) -> Void
+    typealias SegueCompletionBlock<T: UIViewController> = (UIStoryboardSegue, T) -> Void
 }
 
 extension SeguePerformable where Self: UIViewController, SegueIdentifier.RawValue == String {
 
-    func performSegue(_ segueIdentifier: SegueIdentifier, completion: SegueCompletionBlock? = nil) {
+     /**
+     Performs a segue navigation with a completion block
+     */
+    func performSegue<T: UIViewController>(_ type: T.Type,_ segueIdentifier: SegueIdentifier, completion: SegueCompletionBlock<T>? = nil) {
         performSegue(withIdentifier: segueIdentifier.rawValue, sender: completion)
     }
-
+    
+    /**
+     Checks if the sender param on the prepare(for segue:sender:) function is of type SegueCompletionBlock
+     */
     func senderIsSegueCompletionBlock(_ sender: Any?, _ segue: UIStoryboardSegue) -> Bool {
-
-        guard let completion = sender as? SegueCompletionBlock else {
-            return false
-        }
+        guard let completion = sender as? SegueCompletionBlock else { return false }
         completion(segue, segue.destination)
         return true
     }
-
+    
     /**
      Fetches the segue id for a given storyboard and returns its as an enum value
      */
     func segueIdentifier(for segue: UIStoryboardSegue) -> SegueIdentifier {
-
         guard
             let identifier = segue.identifier,
             let segueIdentifier = SegueIdentifier(rawValue: identifier)
             else {
                 fatalError("Invalid segue identifier \(String(describing: segue.identifier))")
         }
-
+        
         return segueIdentifier
     }
 }
